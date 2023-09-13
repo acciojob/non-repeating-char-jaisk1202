fcbebe9fca3454ae3fc209fb284ca96dceb9d978
jaisk1202 <137303123+jaisk1202@users.noreply.github.com>
2023-09-13 12:25:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+ if(!str){
+ return null;
+ }
+
+ const charCount =new Array(26).fill(0);
+
+ for(let i=0;i<str.length;i++){
+ const char=str[i];
+ charCount[char.charCodeAt(0)-'a'.charCodeAt(0)]++;
+ }
+
+ for(let i=0;i<str.length;i++){
+ const char=str[i];
+ if(charCount[char.charCodeAt(0)-'a'.charCodeAt(0)]==1){
+ return char;
+ }
+ }
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
